48d00b663e6d40f96baed900e2341def6f40fa5f
Kyle Finley <git@kylefinley.com>
2014-08-14 19:18:43 -0500

    XXX: temporary hack to get around markdown tempting issue

--- server/error.js
+++ server/error.js
@@ -12,6 +12,12 @@ module.exports = function (err, req, res, next) {
   } else {
     console.log(err.stack || err);
   }
+  // XXX: temporary hack.
+  // Markdown files should be loaded in the db.
+  // Currently tutorials have no way of know if a pages exists until it is render.
+  if (status == 500) {
+    status = 404
+  }
 
   var page = errorApp.createPage(req, res, next);
   page.renderStatic(status, status.toString());
