d0f2b0d0cf86a95b9e30e7ac88c01771acb4dd4a
Kyle Finley <git@kylefinley.com>
2014-08-14 19:18:54 -0500

    started fix for hack

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,10 @@ var express = require('express');
 var sm = require('sitemap');
 var _ = require('lodash');
 var fs = require('fs');
+var marked = require('marked');
+var markedOpts = require('../config/markdown');
+var fs = require('fs');
+
 
 var router = express.Router();
 
@@ -23,6 +27,14 @@ router.all('/tutorials/meteor', function(req, res, next) {
 
 router.all('/tutorials/meteor/blog', function(req, res, next) {
   res.redirect(307, '/tutorials/meteor/blog/1/01');
+});
+
+router.get('/-/update/md', function(req, res, next) {
+  // marked.setOptions(markedOpts);
+  // fs.readFile(__dirname + '/test.md', 'utf8', function(err, text) {
+  //   res.send(text);
+  // });
+  // marked()
 });
 
 
